8712b3c8182ebfb9f87893a4f9c749467831f2c4
EliasFIVE <57570251+EliasFIVE@users.noreply.github.com>
2021-08-14 12:50:46 +0300

    global fix

--- AbaqusDynamicFoundationBC.py
+++ AbaqusDynamicFoundationBC.py
@@ -1,3 +1,4 @@
+from math import sqrt
 import regionToolset
 from abaqusConstants import *
 from caeModules import *
@@ -26,31 +27,43 @@ import numpy as np
 #=====================User Input======================================
 #Data names
 mdbName = 'Dam2D.cae'
-mdbModelName = 'Foundation10000M' #name of target model for creation of BC
+mdbModelName = 'Foundation10000CDS' #name of target model for creation of BC
 partName = 'Foundation10000' 
 borderNodeSet = 'BorderNodes'
-borderOrientation = 'vertical' #vertical, horizontal, sphere 
-odbName = 'SubFoundation10000.odb' #Calcultation result of submodel with unit load and BC from extended faundation
+borderOrientation = 'sphere' #vertical, horizontal, sphere
+csysName = 'CSYSsphere' #for sphere border only 
+OriginOfSphere = [0,0,0] #coordinates of cenntre of sphere, if needed 
+odbName = 'SubFoundation10000S.odb' #Calcultation result of submodel with unit load and BC from extended faundation
 stepName = 'Static'
 
 #multidirectional impacts on extendet model
-#you shoul apply sepparate calculations for ol directoins
+#you should apply sepparate calculations for ol directoins
 multidirectional = True
 stepNameX = stepName + 'X'
 stepNameY = stepName + 'Y'
 
 #MaterialProps
-uniformFaundation = True
+uniformFaundation = True #!!! Only uniform mesh could be applyed for now
 poissonRatio = 0.3
 elasticModulus = 26000000000
 density = 2500
 
 #MeshProps
-uniformMesh = True
-nodeDistance = 400
+uniformMesh = True #!!! Only uniform mesh could be applyed for now
+nodeDistance = 628.2 #for uniform mesh
+
+setupSprings = True
+setupDashpots = True
 #=====================End of user Input======================================
  
 #=====================Utils======================================
+
+def GetCsysByName (name, modelName):
+    assembly = mdb.models[modelName].rootAssembly
+    idIndex = assembly.features[name].id
+    csys = assembly.datums[idIndex]
+    return csys
+
 def GetSubsetFieldValues (fieldOutputName,frame,nodeSetName,csys):
     field = frame.fieldOutputs[fieldOutputName]
     setField = field.getSubset(region=nodeSetName)
@@ -67,31 +80,6 @@ def ValueCombination(Xval,Yval):
     return value
 
 #=====================Main=======================================
-
-# In case of nonuniform foundation damping patamets should calculated for each node separately according to material props
-if (uniformFaundation):
-    shearModulus  = elasticModulus/(2*(1+poissonRatio))
-    Vs = pow(shearModulus/density,0.5)
-    s = 1 - 2*poissonRatio/(2*(1 - poissonRatio))
-    Vc = Vs/s
-    dampN = density*Vc
-    dampT = density*Vs
-    if(borderOrientation == 'vertical'):
-        damp = np.array([dampN, dampT])     #damping for vertical plane border
-    elif(borderOrientation == 'horizontal'):
-        damp = np.array([dampT, dampN])     #damping for horizontal plane border
-    elif(borderOrientation == 'sphere'):
-        print('Sphere border not applyed, set damp=0')  #damping for sphere border
-        damp = 0
-    else:
-        print('ERROR! border type not set, or set incorrectly')
-
-# In case of nonuniform mesh this patamets should calculated for each node separately
-if(uniformMesh):
-    if(nodeDistance == 0):
-        print('ERROR! zero node distance set for uniform mesh')
-    dampNodal = damp*nodeDistance
-
 # Read database
 myMdb = openMdb(mdbName)
 myOdb = visualization.openOdb(path=odbName)
@@ -104,6 +92,13 @@ odbMeshInst = myOdb.rootAssembly.instances[partName.upper()+'-1'].nodeSets[borde
 odbMeshNodes = odbMeshInst.nodes
 print('meshNodes len : ' + str(len(meshNodes)))
 print('odbMeshNodes len : ' + str(len(odbMeshNodes)))
+#Control of mesh nodes and odb nodes matching
+if(len(meshNodes) !=len(odbMeshNodes)):
+    print ('Error!: MeshNodes do not match adbMeshNodes')
+    
+# read sphere local Csys
+if (borderOrientation == 'sphere'):
+    sphereCsys = GetCsysByName(borderOrientation, mdbModelName)
 
 #read odb data for U and RF
 if (multidirectional):
@@ -146,35 +141,15 @@ else:
         dictionaryRFValuesByLabel[label] = np.array(data) 
 print('dictionaryRFValuesByLabel created with len: ' + str(len(dictionaryRFValuesByLabel)))
 
-#not used
-# nodes from model 
-dictionaryMeshNodesByLabel = {
-    node.label: node
-    for node in meshNodes
-}
-print('dictionaryBorderMeshNodesByLabel created with len: ' + str(len(dictionaryMeshNodesByLabel)))
-
-#not used
-#nodes from odb
-dictionaryOdbMeshNodesByLabel = {    
-    node.label: node
-    for node in odbMeshNodes
-}
-print('dictionaryBorderMeshNodesByLabel created with len: ' + str(len(dictionaryOdbMeshNodesByLabel)))
-
-#not used
-#Node sets match control
-dictionaryMeshNodesByLabelSet = set(dictionaryMeshNodesByLabel)
-dictionaryOdbMeshNodesByLabelSet = set(dictionaryOdbMeshNodesByLabel)
-print('Node sets match control:')
-print(dictionaryMeshNodesByLabelSet==dictionaryOdbMeshNodesByLabelSet)
-
+#============Spings param===============
 #calculate spring stiffness
 dictionaryStiffnesByLabel = {}
 for node in meshNodes:
     label = node.label
-    value = abs(dictionaryRFValuesByLabel[node.label]/dictionaryUValuesByLabel[node.label])
+    value = dictionaryRFValuesByLabel[node.label]/dictionaryUValuesByLabel[node.label]  
+    print("label: " + str(node.label) + " RF1: " + str(dictionaryRFValuesByLabel[node.label][0]) + " U1: " + str(dictionaryUValuesByLabel[node.label][0]))
     dictionaryStiffnesByLabel[label] = value
+    print("label: " + str(node.label) + " Stiffnes1: " + str(dictionaryStiffnesByLabel[label][0]))
 
 print('dictionaryStiffnesByLabel created with len: ' + str(len(dictionaryStiffnesByLabel)))
 
@@ -183,8 +158,14 @@ def StiffnesTest2D(labelInt):
     print(dictionaryRFValuesByLabel[labelInt])
     print(dictionaryUValuesByLabel[labelInt])
     print(dictionaryStiffnesByLabel[labelInt])
-    a1 = abs(dictionaryRFValuesByLabel[labelInt][0]/dictionaryUValuesByLabel[labelInt][0]) - dictionaryStiffnesByLabel[labelInt][0]
-    a2 = abs(dictionaryRFValuesByLabel[labelInt][1]/dictionaryUValuesByLabel[labelInt][1]) - dictionaryStiffnesByLabel[labelInt][1]
+    a1test = (dictionaryRFValuesByLabel[labelInt][0]/dictionaryUValuesByLabel[labelInt][0])
+    if (a1test < 0): 
+        a1test = 0
+    if (a2test < 0):
+        a2test = 0
+    a2test = (dictionaryRFValuesByLabel[labelInt][1]/dictionaryUValuesByLabel[labelInt][1])
+    a1 = a1test - dictionaryStiffnesByLabel[labelInt][0]
+    a2 = a2test - dictionaryStiffnesByLabel[labelInt][1]
     if(a1 == 0):
         print('Component 1: Correct')
     else:
@@ -198,62 +179,108 @@ def StiffnesTest2D(labelInt):
 #print('Stiffnes control:')
 #StiffnesTest2D(1)
 
-dictionaryDampByLabel = {}
-#calculate damping by nodal material
-#scal damping by nodal area
-#fill in the dictionary
-for node in meshNodes:
-    dictionaryDampByLabel[node.label] = dampNodal
-print('dictionaryDampByLabel created with len: ' + str(len(dictionaryDampByLabel)))
+#============Damp param===============
 
+# Calculate normal and tangential damping
+# In case of nonuniform foundation damping patamets should calculated for each node separately 
+# according to material props
+if (uniformFaundation):
+    shearModulus  = elasticModulus/(2*(1+poissonRatio))
+    Vs = pow(shearModulus/density,0.5)
+    s = 1 - 2*poissonRatio/(2*(1 - poissonRatio))
+    Vc = Vs/s
+    dampN = density*Vc
+    dampT = density*Vs
+
+dictionaryNodalDampByNodeLabel = {}
+for node in meshNodes:
+    if(borderOrientation == 'vertical'):            #damping for vertical plane border
+        nodalDamp = np.array([dampN, dampT])     
+    elif(borderOrientation == 'horizontal'):        #damping for horizontal plane border
+        nodalDamp = np.array([dampT, dampN])     
+    elif(borderOrientation == 'sphere'):            #damping for sphere border
+        nodalDamp = np.array([dampN, dampT])
+        # nodeCoord = node.coordinates
+        # tgTheta = abs(nodeCoord[0]/nodeCoord[1])
+        # nodalDampX = (dampN*tgTheta+dampT)/sqrt(1+tgTheta*tgTheta)
+        # nodalDampY = (dampT*tgTheta+dampN)/sqrt(1+tgTheta*tgTheta)
+        # nodalDamp = np.array([nodalDampX,nodalDampY])
+    else:
+        print('ERROR! border type not set, or set incorrectly')
+    dictionaryNodalDampByNodeLabel[node.label] = nodalDamp
+print('dictionaryNodalDampByNodeLabel: ' + str(len(dictionaryNodalDampByNodeLabel)))
+
+# In case of nonuniform mesh this patamets should calculated for each node separately
+if(uniformMesh):
+    for node in meshNodes:
+        dictionaryNodalDampByNodeLabel[node.label] *= nodeDistance
+
+def CreateSprings():    
+    for node in meshNodes:
+        meshNodeObj = instanceNodes.sequenceFromLabels((node.label,))
+        region = regionToolset.Region(nodes=meshNodeObj)
+    
+        # get stiffnes
+        KX = -dictionaryStiffnesByLabel[node.label][0]
+        KY = -dictionaryStiffnesByLabel[node.label][1]
+
+        # makes negative values equal to zero
+        if (KX <= 0):
+            KX = 1
+        if (KY <= 0):
+            KY = 1
+
+        # create engineeringFeatures Object (service)
+        sFeatures = myMdb.models[mdbModelName].rootAssembly.engineeringFeatures
+    
+        # create spring-dashpot element
+        springName1 = 'Spr1N' + str(node.label)
+        springName2 = 'Spr2N' + str(node.label)    
+        spring1 = sFeatures.SpringDashpotToGround(name=springName1, region = region, dof=1, orientation=None, springBehavior=ON, dashpotBehavior=OFF, springStiffness=KX)
+        spring2 = sFeatures.SpringDashpotToGround(name=springName2, region = region, dof=2, orientation=None, springBehavior=ON, dashpotBehavior=OFF, springStiffness=KY)
+    
+        eFeatureList.append(spring1)
+        eFeatureList.append(spring2)
+    print('Springs created')
+
+def CreateDashpots():
+    for node in meshNodes:
+        meshNodeObj = instanceNodes.sequenceFromLabels((node.label,))
+        region = regionToolset.Region(nodes=meshNodeObj)
+
+        # get damping
+        DX = dictionaryNodalDampByNodeLabel[node.label][0]
+        DY = dictionaryNodalDampByNodeLabel[node.label][1]
+
+        # create engineeringFeatures Object (service)
+        dFeatures = myMdb.models[mdbModelName].rootAssembly.engineeringFeatures
+    
+        # create spring-dashpot element
+        dashpotName1 = 'Dsh1N' + str(node.label)
+        dashpotName2 = 'Dsh2N' + str(node.label)
+
+        if (borderOrientation == 'sphere'):
+            orientation = sphereCsys
+        else:
+            orientation = None  
+
+        dashpot1 = dFeatures.SpringDashpotToGround(name=dashpotName1, region = region, dof=1, orientation=orientation, springBehavior=OFF, dashpotBehavior=ON, dashpotCoefficient=DX)
+        dashpot2 = dFeatures.SpringDashpotToGround(name=dashpotName2, region = region, dof=2, orientation=orientation, springBehavior=OFF, dashpotBehavior=ON, dashpotCoefficient=DY)
+    
+        eFeatureList.append(dashpot1)    
+        eFeatureList.append(dashpot2)
+
+    print('Dashpots created')
+
+def ClearSpringDashpots():
+    for eFeature in eFeatureList:
+         del(eFeature)
+    eFeatureList.clear()    
+    
 #create eFeatures
 eFeatureList = []
-for node in meshNodes:
-    meshNodeObj = instanceNodes.sequenceFromLabels((node.label,))
-    region = regionToolset.Region(nodes=meshNodeObj)
-    
-    # get stiffnes
-    KX = dictionaryStiffnesByLabel[node.label][0]
-    KY = dictionaryStiffnesByLabel[node.label][1]
-    
-    # create engineeringFeatures Object (service)
-    sFeatures = myMdb.models[mdbModelName].rootAssembly.engineeringFeatures
-    
-    # create spring-dashpot
-    springName1 = 'Spr1N' + str(node.label)
-    springName2 = 'Spr2N' + str(node.label)    
-    spring1 = sFeatures.SpringDashpotToGround(name=springName, region = region, dof=1, orientation=None, springBehavior=ON, dashpotBehavior=OFF, springStiffness=KX)
-    spring2 = sFeatures.SpringDashpotToGround(name=springName, region = region, dof=2, orientation=None, springBehavior=ON, dashpotBehavior=OFF, springStiffness=KY)
-    
-    eFeatureList.append(spring2)
-    eFeatureList.append(spring1)
-
-#create dashpots
-for node in meshNodes:
-    meshNodeObj = instanceNodes.sequenceFromLabels((node.label,))
-    region = regionToolset.Region(nodes=meshNodeObj)
-
-    # get stiffnes
-    DX = dictionaryDampByLabel[node.label][0]
-    DY = dictionaryDampByLabel[node.label][1]
-
-    # create engineeringFeatures Object (service)
-    dFeatures = myMdb.models[mdbModelName].rootAssembly.engineeringFeatures
-    
-    # create spring-dashpot
-    dashpotName1 = 'Dsh1N' + str(node.label)
-    dashpotName2 = 'Dsh2N' + str(node.label)
-    if(borderOrientation == 'sphere'): 
-        print('Sphere border not applyed')
-    else:   
-        dashpot1 = dFeatures.SpringDashpotToGround(name=dashpotName, region = region, dof=1, orientation=None, springBehavior=OFF, dashpotBehavior=ON, dashpotCoefficient=DX)
-        dashpot2 = dFeatures.SpringDashpotToGround(name=dashpotName, region = region, dof=2, orientation=None, springBehavior=OFF, dashpotBehavior=ON, dashpotCoefficient=DY)
-    
-    eFeatureList.append(dashpot1)    
-    eFeatureList.append(dashpot2)
-
-#def ClearSpringDashpots():
-#    for eFeature in eFeatureList:
-#    eFeatureList.clear()    
-    
-    
+if(setupSprings):
+    CreateSprings()
+if(setupDashpots):
+    CreateDashpots()
+print('Total eFeature elements ' + str(len(eFeatureList)) + ' were created for model ' + mdbModelName)
